7c29df1721201c1f246a17e45b7588ca6bf66eb5
dulanjananisal <dulanjananisal67@gmail.com>
2026-04-11 01:31:07 +0530

    create router folder

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+//routers
+app.use('/api/v1/user', )
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
