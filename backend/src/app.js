1949537067292a8f984d34aab24bffc4f8203e18
dulanjananisal <dulanjananisal67@gmail.com>
2026-04-11 01:26:59 +0530

    create sample code in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,9 @@
-console.log('Start backend!')
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
