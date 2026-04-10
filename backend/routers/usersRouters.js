bfa3067fa038f13141ca16d3707bd178e8c690da
dulanjananisal <dulanjananisal67@gmail.com>
2026-04-11 01:41:47 +0530

    connect user signin and signup routers and controllers with app.js

--- backend/routers/usersRouters.js
+++ backend/routers/usersRouters.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { userSignup } = require('../controllers/usersController');
+const { userSignup, userSignin } = require('../controllers/usersController');
 const router = express.Router()
 
 router.route('/signup').get(userSignup);
+router.route('/signin').get(userSignin);
 
 module.exports = router
