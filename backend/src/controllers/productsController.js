2e0de3d15c401b6605594cea873b618a66015b03
dulanjananisal <dulanjananisal67@gmail.com>
2026-04-12 21:12:27 +0530

    remove all product data from product json file and add new products data to it, and aslo upgrade some functionality for products api in product controller

--- backend/src/controllers/productsController.js
+++ backend/src/controllers/productsController.js
@@ -7,35 +7,59 @@ const statusCodes = require('http-status-codes');
 //get all products
 const getAllProducts = asyncHaddler(async(req,res)=>{
 
-    //get all product with pageing (page=page_number, limit=1)
-    const page = req.query.page || 1;
-    const limit = req.query.limit || 10;
+    const {search,category,limit,page,sortBy,brand,price,rating} = req.query;
 
-    const skip = (page-1)*limit
+    let queryObject = {}; 
 
-    //get catoegory
-    const category = req.query.category || "";
+    //products filter by serach
+    if(search){
+        queryObject.name = { $regex:search, $options: 'i' };
+    }
+    //product filter by brand
+    if(brand){
+        queryObject.brand = brand;
+    }
+    //product filter by category
+    if(category){
+        queryObject.category = category;
+
+    }
+    const result = Products.find(queryObject);
+
+    //product sortby price
+    if(sortBy){
+        if(sortBy === 'low_to_high'){
+            result.sort({price: 1})
+        }
+        if(sortBy === 'high_to_low'){
+            result.sort({price: -1})
+        }
+    }
+
+    //product paging
+    const pageCount = page || 1;
+    const limitSize = limit || 10;
+    const skip = (pageCount-1)*limitSize
+
+    //get products
+    const allProducts = await result
+        .skip(skip)
+        .limit(limit)
 
     //count all products
-    const allProductCount = await Products.find();
-
-    const allProducts = await Products
-                                .find({category: category})
-                                .sort({updatedAt: -1})
-                                .skip(skip)
-                                .limit(limit)
-
+    const allProductCount = await Products.find(queryObject);
     res.status(statusCodes.OK).json({
         success: true,
         result_count: allProductCount.length,
         products: allProducts,
-        page: page,
+        page: pageCount,
     })
 })
 
 //add products
 const addProducts = asyncHaddler(async(req,res)=>{
-    res.status(200).send('Add Products!')
+    await Products.create(req.body)
+    res.status(statusCodes.OK).send("Product Added!")
 })
 
 //get single products
