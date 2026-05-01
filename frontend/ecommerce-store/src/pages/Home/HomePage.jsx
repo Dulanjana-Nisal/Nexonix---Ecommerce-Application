3538b9c13c54dfc61bb8c5030cb955204bbcea27
dulanjananisal <dulanjananisal67@gmail.com>
2026-05-01 18:59:29 +0530

    add header component and add styles for it

--- frontend/ecommerce-store/src/pages/Home/HomePage.jsx
+++ frontend/ecommerce-store/src/pages/Home/HomePage.jsx
@@ -1,95 +1,11 @@
+import HeaderComponent from '../../components/Header/HeaderComponent'
 import './HomePage.css'
 
 function HomePage() {
     return (
         <>
             {/* <!---------------- Header ----------------> */}
-            <div class="header">
-                <div class="header-top">
-                    <div class="header-top-left">
-                        <img src="./images/logo2.png" alt="logo" />
-                    </div>
-                    <div class="header-top-center">
-                        <div class="search-box">
-                            <input type="text" placeholder="Search for products..." />
-                            <button>Search</button>
-                        </div>
-                        <div class="result-box">
-                            <div class="result">
-                                <h3>Razer BlackWidow V4 Pro</h3>
-                                <p>gamings</p>
-                            </div>
-                            <div class="result">
-                                <h3>Razer BlackWidow V4 Pro</h3>
-                                <p>gamings</p>
-                            </div>
-                            <div class="result">
-                                <h3>Razer BlackWidow V4 Pro</h3>
-                                <p>gamings</p>
-                            </div>
-                            <div class="result">
-                                <h3>Razer BlackWidow V4 Pro</h3>
-                                <p>gamings</p>
-                            </div>
-                            <div class="result-btn">
-                                <button>See all</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="header-top-right">
-                        <div class="orders">
-                            <p>Orders</p>
-                        </div>
-                        <div class="profile">
-                            <div class="profile-left">
-                                <img src="./images/user-profile.png" alt="user-profile" />
-                            </div>
-                            <div class="profile-right">
-                                <p>Login</p>
-                                <h3>Account</h3>
-                            </div>
-                        </div>
-                        <div class="cart">
-                            <div class="cart-left">
-                                <img src="./images/shopping-cart.png" alt="shopping-cart" />
-                                <p>0</p>
-                            </div>
-                            <div class="cart-right">
-                                <p>Your Cart</p>
-                                <h3>$0.00</h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <hr />
-                <div class="header-navbar">
-                    <div class="navbar-left">
-                        <div class="navbar-left-selection">
-                            <img src="./images/hamberger-menu.png" alt="" />
-                            <p>Browse All Categories</p>
-                        </div>
-                        <div class="navbar-left-selection-box">
-                            <ul>
-                                <li><a href="#">Computers</a></li>
-                                <li><a href="#">Laptops</a></li>
-                                <li><a href="#">Components</a></li>
-                                <li><a href="#">Gamings</a></li>
-                                <li><a href="#">Softwares</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="navbar-right">
-                        <ul>
-                            <li><a href="#">Computers</a></li>
-                            <li><a href="#">Laptops</a></li>
-                            <li><a href="#">Components</a></li>
-                            <li><a href="#">Gamings</a></li>
-                            <li><a href="#">Softwares</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <hr />
-            </div>
+            <HeaderComponent />
             {/* ---------------- container ---------------- */}
             <div class="container">
                 {/* <!-- Banner --> */}
