0b7960ef13cb50c931f08f440a25fa6691aa1fb1
dulanjananisal <dulanjananisal67@gmail.com>
2026-05-29 22:54:10 +0530

    add orders data into state array and mapping it

--- frontend/ecommerce-store/src/pages/Orders/OrdersPage.jsx
+++ frontend/ecommerce-store/src/pages/Orders/OrdersPage.jsx
@@ -42,111 +42,72 @@ function OrdersPage() {
                             <p class="delete"></p>
                         </div>
                         <div class="details-cards">
-                            <div class="card">
-                                <div class="card-product product">
-                                    <div class="thumb">
-                                        <img src="../../images/card-image.png" alt="" />
-                                    </div>
-                                    <div class="name">
-                                        <p>Lenovo Legion 7i Gen 9 Laptop</p>
-                                        <p class="order-id">ID: 69dbab0ed4d8c035adc7624e</p>
-                                    </div>
-                                </div>
-                                <div class="card-date date">
-                                    <p>2026.02.12</p>
-                                </div>
-                                <div class="card-price price">
-                                    <p>$234.34</p>
-                                </div>
-                                <div class="card-quantity quantity">
-                                    <p>1</p>
-                                </div>
-                                <div class="card-status status">
-                                    <p class="deliverd">Deliverd</p>
-                                </div>
-                                <button class="delete"><img src="../../images/delete-icon.png" alt="" /></button>
-                            </div>
-                            <div class="card">
-                                <div class="card-product product">
-                                    <div class="thumb">
-                                        <img src="../../images/card-image.png" alt="" />
-                                    </div>
-                                    <div class="name">
-                                        <p>Lenovo Legion 7i Gen 9 Laptop</p>
-                                        <p class="order-id">(ID: 12322344331)</p>
-                                    </div>
-                                </div>
-                                <div class="card-date date">
-                                    <p>2026.02.12</p>
-                                </div>
-                                <div class="card-price price">
-                                    <p>$234.34</p>
-                                </div>
-                                <div class="card-quantity quantity">
-                                    <p>1</p>
-                                </div>
-                                <div class="card-status status">
-                                    <p class="deliverd">Deliverd</p>
-                                </div>
-                                <button class="delete"><img src="../../images/delete-icon.png" alt="" /></button>
-                            </div>
+                            {
+                                orders.map((items)=>{
+                                    return(
+                                        <div class="card" key={items._id}>
+                                            <div class="card-product product">
+                                                <div class="thumb">
+                                                    <img src="../../images/card-image.png" alt="" />
+                                                </div>
+                                                <div class="name">
+                                                    <p>Lenovo Legion 7i Gen 9 Laptop</p>
+                                                    <p class="order-id">ID: 69dbab0ed4d8c035adc7624e</p>
+                                                </div>
+                                            </div>
+                                            <div class="card-date date">
+                                                <p>2026.02.12</p>
+                                            </div>
+                                            <div class="card-price price">
+                                                <p>$234.34</p>
+                                            </div>
+                                            <div class="card-quantity quantity">
+                                                <p>1</p>
+                                            </div>
+                                            <div class="card-status status">
+                                                <p class="deliverd">Deliverd</p>
+                                            </div>
+                                            <button class="delete"><img src="../../images/delete-icon.png" alt="" /></button>
+                                        </div>
+                                    )
+                                })
+                            }
                         </div>
                     </div>
                     <div class="order-details-responsive">
-                        <div class="card">
-                            <div class="image">
-                                <img src="../../images/card-image.png" alt="" />
-                            </div>
-                            <div class="card-details">
-                                <div class="name">
-                                    <h1>Lenovo Legion 7i Gen 9 Laptop</h1>
-                                    <button><img src="../../images/delete-icon.png" alt="" /></button>
-                                </div>
-                                <div class="price">
-                                    <p>Price: </p>
-                                    <h3>$234.44</h3>
-                                </div>
-                                <div class="quantity">
-                                    <div class="quentity-header">
-                                        <p>Quantity: </p>
+                        {
+                            orders.map((items)=>{
+                                return(
+                                    <div class="card" key={items._id}>
+                                        <div class="image">
+                                            <img src="../../images/card-image.png" alt="" />
+                                        </div>
+                                        <div class="card-details">
+                                            <div class="name">
+                                                <h1>Lenovo Legion 7i Gen 9 Laptop</h1>
+                                                <button><img src="../../images/delete-icon.png" alt="" /></button>
+                                            </div>
+                                            <div class="price">
+                                                <p>Price: </p>
+                                                <h3>$234.44</h3>
+                                            </div>
+                                            <div class="quantity">
+                                                <div class="quentity-header">
+                                                    <p>Quantity: </p>
+                                                </div>
+                                                <div class="quentity-body">
+                                                    <p>1</p>
+                                                </div>
+                                            </div>
+                                            <div class="subtotal">
+                                                <p>Subtotal: </p>
+                                                <h3>$234.44</h3>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div class="quentity-body">
-                                        <p>1</p>
-                                    </div>
-                                </div>
-                                <div class="subtotal">
-                                    <p>Subtotal: </p>
-                                    <h3>$234.44</h3>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="image">
-                                <img src="../../images/card-image.png" alt="" />
-                            </div>
-                            <div class="card-details">
-                                <div class="name">
-                                    <h1>Lenovo Legion 7i Gen 9 Laptop</h1>
-                                    <button><img src="../../images/delete-icon.png" alt="" /></button>
-                                </div>
-                                <div class="price">
-                                    <p>Price: </p>
-                                    <h3>$234.44</h3>
-                                </div>
-                                <div class="quantity">
-                                    <div class="quentity-header">
-                                        <p>Quantity: </p>
-                                    </div>
-                                    <div class="quentity-body">
-                                        <p>1</p>
-                                    </div>
-                                </div>
-                                <div class="subtotal">
-                                    <p>Subtotal: </p>
-                                    <h3>$234.44</h3>
-                                </div>
-                            </div>
-                        </div>
+                                )
+                            })
+                        }
                     </div>
                 </div>
             </div>
