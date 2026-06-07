1a87b9f856dddb2ef27931b733c20c8915d56856
dulanjananisal <dulanjananisal67@gmail.com>
2026-06-07 14:29:40 +0530

    add delete method for review controller

--- backend/src/controllers/rewiewController.js
+++ backend/src/controllers/rewiewController.js
@@ -1,10 +1,38 @@
+const NotFoundErrorHaddler = require('../errors/NotFoundErrorHaddler');
 const Reviews = require('../models/reviewsModel');
 const asyncHaddler = require('../utils/asyncHaddler');
 
 // get all reviews
 const getAllReviews = asyncHaddler(async (req,res) => {
-    const getAllReviews = await Reviews.find({})
-    res.status(200).json({success: true, data: getAllReviews})
+    //get parem data
+    const {productId,userId} = req.query
+
+    //query selectore
+    let querySelectore = {}
+
+    // find reviews with product id
+    if(productId){
+        querySelectore.productId = productId
+    }
+
+    //find reviews with userId
+    if(userId){
+        querySelectore.userId = userId
+    }
+
+    //product paging
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page-1)*limit
+
+    //fetch reviews in db
+    const allReviews = await Reviews.find(querySelectore).skip(skip).limit(limit).sort({createdAt: -1})
+    res.status(200).json({
+        success: true,
+        all_result: allReviews.length,
+        data: allReviews,
+        page: page,
+    })
 })
 
 //post reviews
@@ -14,7 +42,19 @@ const postReviews = asyncHaddler(async (req,res) => {
     res.status(201).json({success: true, message: 'Review Created!', data: postReview})
 })
 
+// delete review
+const deleteReview = asyncHaddler( async (req,res) => {
+    const reviewId = req.params.id
+    const deleteOne = await Reviews.findOneAndDelete({_id: reviewId})
+
+    if(!deleteOne){
+        throw new NotFoundErrorHaddler('Product not found!');
+    }
+    res.status(200).json({success: true, message: 'Review Deleted!', data: deleteOne})
+})
+
 module.exports = {
     getAllReviews,
-    postReviews
+    postReviews,
+    deleteReview
 };
--- backend/src/routers/rewiewRouter.js
+++ backend/src/routers/rewiewRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const {getAllReviews,postReviews} = require('../controllers/rewiewController')
+const {getAllReviews,postReviews,deleteReview} = require('../controllers/rewiewController')
 const authenticationMiddleware = require('../middlewares/authenticationMiddleware')
 const router = express.Router();
 
 router.route('/').get(getAllReviews).post(authenticationMiddleware, postReviews);
+router.route('/:id').delete(deleteReview);
 
 module.exports = router;
