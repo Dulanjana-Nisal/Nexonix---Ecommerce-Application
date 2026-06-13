dbd7cb521f4213498dd8bb81b3a9b4e7d86a918d
dulanjananisal <dulanjananisal67@gmail.com>
2026-06-14 00:03:42 +0530

    add two more controllers for users, udpate user and delete users

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,10 +1,64 @@
+const BadrequestErrorHaddler = require('../errors/BadrequestErrorHaddler')
+const NotFoundErrorHaddler = require('../errors/NotFoundErrorHaddler')
+const UnauthorizedErrorHaddler = require('../errors/UnauthorizedErrorHaddler')
 const Users = require('../models/usersModule')
+const asyncHaddler = require('../utils/asyncHaddler')
 
-const getAllUsers = async(req,res) => {
-    const allUsers = await Users.find({})
-    res.status(200).json({success: true, data: allUsers})
-}
+//get all users data
+const getAllUsers = asyncHaddler(async(req,res) => {
+    const {search} = req.query
+
+    const querySelectore = {}
+
+    if(search){
+        querySelectore.name = { $regex:search, $options: 'i' };
+    }
+    //product paging
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page-1)*limit
+
+    const allUsers = await Users.find(querySelectore, {password: 0}).skip(skip).limit(limit)
+    res.status(200).json({success: true,page: page, data: allUsers})
+})
+
+//udpate users
+const updateUsers = asyncHaddler(async(req,res)=>{
+    const userId = req.params.id
+    if("password" in req.body){
+        throw new BadrequestErrorHaddler('Cant update password!')
+    }
+    if("_id" in req.body || "email" in req.body){
+        throw new BadrequestErrorHaddler('Cant update ID or Email')
+    }
+    if(req.user.role !== "admin"){
+        throw new UnauthorizedErrorHaddler('Not authorized')
+    }
+    const updateUser = await Users.findOneAndUpdate({_id: userId}, req.body,{runValidators: true, returnDocument: 'after'})
+
+    if(!updateUser){
+        throw new NotFoundErrorHaddler('User not found!')
+    }
+
+    res.status(200).json({success: true, data: updateUser})
+})
+
+//delete users 
+const deleteUsers = asyncHaddler(async(req,res)=>{
+    const userId = req.params.id 
+    if(req.user.role !== "admin"){
+        throw new UnauthorizedErrorHaddler('Not authorized!')
+    }
+    const deleteUser = await Users.findOneAndDelete({_id: userId})
+    if(!deleteUser){
+        throw new NotFoundErrorHaddler('User not found!')
+    }
+    res.status(200).json({success: true, data: deleteUser})
+})
 
 module.exports = {
-    getAllUsers
+    getAllUsers,
+    deleteUsers,
+    updateUsers,
+    deleteUsers
 }
--- backend/src/routers/usersRouter.js
+++ backend/src/routers/usersRouter.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const getAllUsers = require('../controllers/userController')
+const {getAllUsers,deleteUsers,updateUsers} = require('../controllers/userController');
+const authenticationMiddleware = require('../middlewares/authenticationMiddleware');
 
 const router = express.Router();
 
 router.route('/').get(getAllUsers)
+router.route('/:id').patch(authenticationMiddleware, updateUsers).delete(authenticationMiddleware, deleteUsers)
 
 module.exports = router
