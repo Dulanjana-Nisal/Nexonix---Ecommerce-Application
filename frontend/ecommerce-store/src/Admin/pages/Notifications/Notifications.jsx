1755fe9a8c030dfee9cc146a18800448ea3e9b0f
dulanjananisal <dulanjananisal67@gmail.com>
2026-06-18 22:13:11 +0530

    load notifications from db

--- frontend/ecommerce-store/src/Admin/pages/Notifications/Notifications.jsx
+++ frontend/ecommerce-store/src/Admin/pages/Notifications/Notifications.jsx
@@ -1,6 +1,41 @@
+import { useEffect, useState } from 'react';
 import './Notifications.css';
+import api from '../../../services/auth'
 
 function Notifications() {
+
+    // notification states
+    const [notifications, setNotifications] = useState([])
+
+    //useEffect for notifications
+    useEffect(() => {
+        const fetchNotificationsData = async () => {
+            const reuslt = await api.get(`/notifications`)
+            setNotifications(reuslt.data.data)
+        }
+        fetchNotificationsData()
+    }, [])
+
+    console.log(notifications)
+
+    // set notification UI
+    const notificationThumb = {
+        "users": <div class="notif-icon info">
+                    <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" /></svg>
+                </div>,
+        "products": <div class="notif-icon warning">
+                        <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+                    </div>,
+        "orders": <div class="notif-icon success">
+                        <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                    </div>
+    }
+    const notificationTag = {
+        "users": "badge-pill info",
+        "products": "badge-pill warning",
+        "orders": "badge-pill success"
+    }
+
     return (
         <>
             {/* <!-- Container --> */}
@@ -23,37 +58,45 @@ function Notifications() {
                         </div>
                         <div class="notif-list">
 
-                            {/* <!-- Item 1 --> */}
-                            <div class="notif-item">
-                                <div class="unread-dot"></div>
-                                <div class="notif-icon info">
-                                    <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" /></svg>
-                                </div>
-                                <div class="notif-body">
-                                    <div class="notif-title">New user registered</div>
-                                    <div class="notif-desc">A new user "Sarah Johnson" has registered.</div>
-                                    <div class="notif-time">2 minutes ago</div>
-                                </div>
-                                <div class="notif-right">
-                                    <span class="badge-pill info">Users</span>
-                                    <button class="menu-btn">
-                                        <svg width="16" height="16" fill="currentColor" viewBox="0 0 24 24"><circle cx="12" cy="5" r="1.5" /><circle cx="12" cy="12" r="1.5" /><circle cx="12" cy="19" r="1.5" /></svg>
-                                    </button>
-                                    <div class="actions-box" style={{display: 'none'}}>
-                                        <div class='read'>
-                                            <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" width="14" height="14"><path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" /></svg>
-                                            <p >Mark as Read</p>
+                            {
+                                notifications.length > 0 &&
+                                notifications.map((items) => {
+                                    return (
+                                        <div class="notif-item" key={items._id}>
+                                            {
+                                                !items.isRead &&
+                                                <div class="unread-dot"></div>
+                                            }
+                                            {
+                                                notificationThumb[items.type]
+                                            }
+                                            <div class="notif-body">
+                                                <div class="notif-title">{items.title}</div>
+                                                <div class="notif-desc">{items.message}</div>
+                                                <div class="notif-time">2 minutes ago</div>
+                                            </div>
+                                            <div class="notif-right">
+                                                <span class={notificationTag[items.type]}>{items.type}</span>
+                                                <button class="menu-btn">
+                                                    <svg width="16" height="16" fill="currentColor" viewBox="0 0 24 24"><circle cx="12" cy="5" r="1.5" /><circle cx="12" cy="12" r="1.5" /><circle cx="12" cy="19" r="1.5" /></svg>
+                                                </button>
+                                                <div class="actions-box" style={{ display: 'none' }}>
+                                                    <div class='read'>
+                                                        <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" width="14" height="14"><path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" /></svg>
+                                                        <p >Mark as Read</p>
+                                                    </div>
+                                                    <div class='delete'>
+                                                        <svg width="14" height="14" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15" stroke-linecap="round"></line><line x1="9" y1="9" x2="15" y2="15" stroke-linecap="round"></line></svg>
+                                                        <p>Delete Notification</p>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
-                                        <div class='delete'>
-                                            <svg width="14" height="14" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15" stroke-linecap="round"></line><line x1="9" y1="9" x2="15" y2="15" stroke-linecap="round"></line></svg>
-                                            <p>Delete Notification</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                                    )
+                                })
+                            }
 
-                            {/* <!-- Item 2 --> */}
-                            <div class="notif-item">
+                            {/* <div class="notif-item">
                                 <div class="unread-dot"></div>
                                 <div class="notif-icon success">
                                     <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
@@ -71,7 +114,7 @@ function Notifications() {
                                 </div>
                             </div>
 
-                            {/* <!-- Item 3 --> */}
+
                             <div class="notif-item">
                                 <div class="unread-dot"></div>
                                 <div class="notif-icon warning">
@@ -90,7 +133,7 @@ function Notifications() {
                                 </div>
                             </div>
 
-                            {/* <!-- Item 1 --> */}
+
                             <div class="notif-item">
                                 <div class="unread-dot"></div>
                                 <div class="notif-icon info">
@@ -109,7 +152,7 @@ function Notifications() {
                                 </div>
                             </div>
 
-                            {/* <!-- Item 2 --> */}
+
                             <div class="notif-item">
                                 <div class="unread-dot"></div>
                                 <div class="notif-icon success">
@@ -128,7 +171,7 @@ function Notifications() {
                                 </div>
                             </div>
 
-                            {/* <!-- Item 3 --> */}
+
                             <div class="notif-item">
                                 <div class="unread-dot"></div>
                                 <div class="notif-icon warning">
@@ -145,7 +188,7 @@ function Notifications() {
                                         <svg width="16" height="16" fill="currentColor" viewBox="0 0 24 24"><circle cx="12" cy="5" r="1.5" /><circle cx="12" cy="12" r="1.5" /><circle cx="12" cy="19" r="1.5" /></svg>
                                     </button>
                                 </div>
-                            </div>
+                            </div> */}
 
                         </div>
                     </div>
