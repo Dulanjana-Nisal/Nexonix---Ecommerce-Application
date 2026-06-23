0eb284a5f0878e10eb58504d159f684646f1c9d6
dulanjananisal <dulanjananisal67@gmail.com>
2026-06-23 14:27:56 +0530

    upgrade home page info section

--- frontend/ecommerce-store/src/pages/Home/Info.jsx
+++ frontend/ecommerce-store/src/pages/Home/Info.jsx
@@ -1,14 +1,12 @@
-import delivery_thumb from '../../assets/delevery-thumb.png';
-import payment_thumb from '../../assets/payment-thumb.png';
-import products_thumb from '../../assets/products-thumb.png';
-
 function Info() {
     return (
         <>
             <div class="info">
                 <div class="info-box">
                     <div class="box-img">
-                        <img src={delivery_thumb} alt="info-thumb" />
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M1 3h13v13H1z" /><path d="M14 8h4l3 3v5h-7z" /><circle cx="6" cy="18.5" r="1.7" /><circle cx="17.5" cy="18.5" r="1.7" />
+                        </svg>
                     </div>
                     <div class="box-data">
                         <h4>Fast Delivery</h4>
@@ -17,7 +15,9 @@ function Info() {
                 </div>
                 <div class="info-box">
                     <div class="box-img">
-                        <img src={payment_thumb} alt="info-thumb" />
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M12 2l8 3.5v6c0 5-3.6 8.3-8 10.5-4.4-2.2-8-5.5-8-10.5v-6L12 2z" /><path d="M8.5 12l2.3 2.3L15.5 9.7" />
+                        </svg>
                     </div>
                     <div class="box-data">
                         <h4>Safe Payment</h4>
@@ -26,11 +26,24 @@ function Info() {
                 </div>
                 <div class="info-box">
                     <div class="box-img">
-                        <img src={products_thumb} alt="info-thumb" />
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                            <circle cx="12" cy="8" r="5.5" /><path d="M8.3 12.8L7 21l5-2.6L17 21l-1.3-8.2" />
+                        </svg>
                     </div>
                     <div class="box-data">
                         <h4>Best Products</h4>
                         <p>Selling Top Rated Products</p>
+                    </div>
+                </div>
+                <div class="info-box">
+                    <div class="box-img">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M3 13a9 9 0 0 1 18 0" /><path d="M3 13v4a2 2 0 0 0 2 2h1v-7H4a1 1 0 0 0-1 1z" /><path d="M21 13v4a2 2 0 0 1-2 2h-1v-7h2a1 1 0 0 1 1 1z" />
+                        </svg>
+                    </div>
+                    <div class="box-data">
+                        <h4>24/7 Support</h4>
+                        <p>We're Here to Help</p>
                     </div>
                 </div>
             </div>
