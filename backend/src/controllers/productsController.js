ba8eb5fb7620f77b7f1e01d9caec58691eb3ff7f
dulanjananisal <dulanjananisal67@gmail.com>
2026-06-24 23:11:00 +0530

    upgrade ui in search and category pages and update some code in backend

--- backend/src/controllers/productsController.js
+++ backend/src/controllers/productsController.js
@@ -44,7 +44,7 @@ const getAllProducts = asyncHaddler(async(req,res)=>{
 
     //product paging
     const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 10;
+    const limit = Number(req.query.limit) || 12;
     const skip = (page-1)*limit
 
     const result = Products.find(queryObject);
