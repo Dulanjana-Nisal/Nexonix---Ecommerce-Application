13d72cc65257ef6e70a4b3d618a144b44d3076c9
dulanjananisal <dulanjananisal67@gmail.com>
2026-07-03 00:05:39 +0530

    remove className proprty

--- frontend/ecommerce-store/src/pages/Home/Category.jsx
+++ frontend/ecommerce-store/src/pages/Home/Category.jsx
@@ -12,52 +12,52 @@ function Category() {
 
     return (
         <>
-            <div className="category-container">
-                <div className="container-header">
+            <div class="category-container">
+                <div class="container-header">
                     <h1>Shop By Category 🎨</h1>
                 </div>
-                <div className="container-footer">
-                    <div className="category-card" onClick={() => navigate('/products/computers')}>
-                        <div className="card-header">
+                <div class="container-footer">
+                    <div class="category-card" onClick={() => navigate('/products/computers')}>
+                        <div class="card-header">
                             <img src={computer_category} alt="" />
                         </div>
-                        <div className="card-bottom">
+                        <div class="card-bottom">
                             <h3>Computers</h3>
                             <p>High performance desktops</p>
                         </div>
                     </div>
-                    <div className="category-card" onClick={() => navigate('/products/laptops')}>
-                        <div className="card-header">
+                    <div class="category-card" onClick={() => navigate('/products/laptops')}>
+                        <div class="card-header">
                             <img src={laptops_category} alt="" />
                         </div>
-                        <div className="card-bottom">
+                        <div class="card-bottom">
                             <h3>Laptops</h3>
                             <p>Portable power for everyone</p>
                         </div>
                     </div>
-                    <div className="category-card" onClick={() => navigate('/products/components')}>
-                        <div className="card-header">
+                    <div class="category-card" onClick={() => navigate('/products/components')}>
+                        <div class="card-header">
                             <img src={component_category} alt="" />
                         </div>
-                        <div className="card-bottom">
+                        <div class="card-bottom">
                             <h3>Components</h3>
                             <p>Build your dream setup</p>
                         </div>
                     </div>
-                    <div className="category-card" onClick={() => navigate('/products/gamings')}>
-                        <div className="card-header">
+                    <div class="category-card" onClick={() => navigate('/products/gamings')}>
+                        <div class="card-header">
                             <img src={gaming_category} alt="" />
                         </div>
-                        <div className="card-bottom">
+                        <div class="card-bottom">
                             <h3>Gamings</h3>
                             <p>Level up your game</p>
                         </div>
                     </div>
-                    <div className="category-card" onClick={() => navigate('/products/softwares')}>
-                        <div className="card-header">
+                    <div class="category-card" onClick={() => navigate('/products/softwares')}>
+                        <div class="card-header">
                             <img src={sonftware_category} alt="" />
                         </div>
-                        <div className="card-bottom">
+                        <div class="card-bottom">
                             <h3>Softwares</h3>
                             <p>Productivity & tools</p>
                         </div>
