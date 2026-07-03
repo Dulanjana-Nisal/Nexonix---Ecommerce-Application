600de21e62b9fe52d373627b29af9b5e96010bac
dulanjananisal <dulanjananisal67@gmail.com>
2026-07-03 17:39:17 +0530

    convert class in to className in MessagesCoponent

--- frontend/ecommerce-store/src/components/Messages/MessagesComonent.jsx
+++ frontend/ecommerce-store/src/components/Messages/MessagesComonent.jsx
@@ -3,9 +3,9 @@ import './MessagesComponent.css';
 function MessagesComponent({ title, message, status }) {
 
     return (
-        <div class={`message-container message-container-${status}`}>
-            <div class="left-side">
-                <div class={`svg-border svg-border-${status}`}>
+        <div className={`message-container message-container-${status}`}>
+            <div className="left-side">
+                <div className={`svg-border svg-border-${status}`}>
                     {
                         status === 'error' ?
                             <svg width="14" height="14" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2.9"><path stroke-linecap="round" stroke-linejoin="round" d="M12 9v2m0 4h.01M10.29 3.86L1.82 18a2 2 0 001.71 3h16.94a2 2 0 001.71-3L13.71 3.86a2 2 0 00-3.42 0z"></path></svg>
@@ -14,11 +14,11 @@ function MessagesComponent({ title, message, status }) {
                     }
                 </div>
             </div>
-            <div class="middle-side">
+            <div className="middle-side">
                 <h3>{title}</h3>
                 <p>{message}</p>
             </div>
-            <div class="right-side">
+            <div className="right-side">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.1" stroke-linecap="round" stroke-linejoin="round">
                     <line x1="18" y1="6" x2="6" y2="18"></line>
                     <line x1="6" y1="6" x2="18" y2="18"></line>
